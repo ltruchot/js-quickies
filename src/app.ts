4ffb177df984fbe39cfad05137fabb4483467ab7
Loic TRUCHOT <loic.truchot.pro@gmail.com>
2018-02-04 23:56:00 +0100

    chore(basics): add basic environment to run & watch code

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+// warning: tc39 stage 2 in february 2018
+// like a decoration: something you wear that enhance yourself 
+// declarative vs imperative programming
+// composition vs inheritance
+// java annotation -> python decorators -> js decorators
+console.log('test 2');
